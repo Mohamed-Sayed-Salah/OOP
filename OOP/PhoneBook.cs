4879e154e55e44bbffbf9f0948775e01bc3d466d
Mohamed Salah <mosayed26303@gmail.com>
2024-12-24 02:49:22 +0200

    Add project files.

---
+++ OOP/PhoneBook.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OOP
+{
+    internal struct PhoneBook
+    {
+        #region Attributes 
+
+        string[] names;
+        long[] numbers;
+        int size;
+
+        #endregion
+
+        #region Properties
+
+        public int Size { get { return size; } }
+
+        #endregion
+
+        #region Constructor
+        public PhoneBook(int _size)
+        {
+            size = _size;
+            names = new string[size];
+            numbers = new long[size];
+        }
+        #endregion
+
+        #region Methods
+
+        public void AddPerson(int position, string name, long number)
+        {
+            if (names is not null && numbers is not null)
+            {
+                if (position >= 0 && position < size)
+                {
+                    names[position] = name;
+                    numbers[position] = number;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Encapsulation 
+
+        #region Getter / setter
+        public long GetNumber(string name)
+        {
+
+            if (names is not null && numbers is not null)
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] == name)
+                        return numbers[i];
+                }
+            }
+            return -1;
+        }
+        public void SetNumber(string name, long number)
+        {
+            if (names is not null && numbers is not null)
+            {
+                if (names.Length > 0 && numbers.Length > 0)
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (names[i] == name)
+                            numbers[i] = number;
+                    }
+                }
+            }
+
+        }
+
+        #endregion
+
+        #region Index
+
+        public long this[string name]
+        {
+            get
+            {
+                if (names is not null && numbers is not null)
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (names[i] == name)
+                            return numbers[i];
+                    }
+                }
+                return -1;
+            }
+
+            set
+            {
+                if (names is not null && numbers is not null)
+                {
+                    if (names.Length > 0 && numbers.Length > 0)
+                    {
+                        for (int i = 0; i < names.Length; i++)
+                        {
+                            if (names[i] == name)
+                                numbers[i] = value;
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Session02 
+
+            #region Demo 
+
+            #region Indexer
+
+            //PhoneBook phoneBook = new PhoneBook(3);
+
+            //phoneBook.AddPerson(0, "ali", 1234);
+            //phoneBook.AddPerson(1, "mo", 5678);
+            //phoneBook.AddPerson(2, "eslam", 9513);
+
+            //phoneBook["ali"] = 100100;
+
+            //Console.WriteLine(phoneBook["ali"]);
+
+            #endregion
+
+            #region Class 
+
+            #endregion
+
+            #region Inheritance 
+
+            #endregion
+
+            #region Relationships Between Classes 
+
+            #endregion
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
